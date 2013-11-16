11cd799d5f70b318900008013f9121548eadb1a1
ybabel <ybabel@free.fr>
2013-11-16 22:51:27 +0100

    Update HelloRouteController.php

--- lib/Drupal/hello/HelloRouteController.php
+++ lib/Drupal/hello/HelloRouteController.php
@@ -7,17 +7,8 @@
  
 namespace Drupal\hello;
  
-/**
- * Returns responses for User module routes.
- */
 class HelloRouteController {
  
-  /**
-   * Returns the route content.
-   *
-   * @return array
-   *   A renderable array containing the page content.
-   */
   public function index() {
     return array('#markup' => 'Hello workd page text (from controller) !');
   }
