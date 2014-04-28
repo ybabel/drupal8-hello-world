8ba00966e884207120cfac43d82e36e90aaacbc6
qdelance <quentin.delance@gmail.com>
2014-04-28 23:46:10 +0200

    Second part of the previous commit

--- lib/Drupal/hello/Form/HelloForm.php
+++ lib/Drupal/hello/Form/HelloForm.php
@@ -7,35 +7,61 @@
  
 namespace Drupal\hello\Form;
  
-use Drupal\Core\Form\FormInterface;
-use Drupal\Core\Form\FormBase; 
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\State\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
-//class HelloForm implements FormInterface {
-class HelloForm extends FormBase {
+class HelloForm extends ConfigFormBase {
  
+  /**
+   * The configuration object.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * Constructs a HelloForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactory $config_factory
+   *   The factory for configuration objects.
+   */
+  public function __construct(ConfigFactory $config_factory) {
+    parent::__construct($config_factory);
+    $this->config = $this->config('hello.settings');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory')
+    );
+  }
+
   public function getFormID() {
     return 'hello_form';
   }
 
   public function buildForm(array $form, array &$form_state) {
-    $form['hello_config'] = array(
+    $form['hello_text'] = array(
       '#type' => 'textfield',
-      '#title' => t('Who are you ?'),
+      '#title' => t('Greeting text ?'),
       '#size' => 60,
-      '#description' => t('This text will appear in the hello world block.'),
-      '#default_value' => config('hello.settings')->get('hello_value'),
+      '#description' => t('This text will be used to greet people in Hello Block.'),
+      '#default_value' => $this->config->get('hello_text'),
     );
-    $form['submit'] = array(
-      '#type' => 'submit',
-      '#value' => t('Save'),
-    );
-    return $form;
+
+    return parent::buildForm($form, $form_state);
   }
  
 
   public function submitForm(array &$form, array &$form_state) {
-    config('hello.settings')
-      ->set('hello_value', $form_state['values']['hello_config'])
+    parent::submitForm($form, $form_state);
+    $this->config
+      ->set('hello_text', $form_state['values']['hello_text'])
       ->save(); 
   }
  
--- lib/Drupal/hello/Plugin/Block/HelloBlock.php
+++ lib/Drupal/hello/Plugin/Block/HelloBlock.php
@@ -10,29 +10,69 @@
 namespace Drupal\hello\Plugin\Block;
  
 use Drupal\block\BlockBase;
-use Drupal\block\Annotation\Block;
+use Drupal\Component\Annotation\Plugin;
+use Drupal\Component\Utility\String;
 use Drupal\Core\Annotation\Translation;
 use Drupal\Core\Session\AccountInterface;
 
 /**
  * Provides a 'Hello' block.
- ** @Block(
+ * @Block(
  *   id = "hello_block",
- *   admin_label = @Translation("Hello world block title")
+ *   admin_label = @Translation("Hello Block Title"),
+ *   module = "hello"
  * )
  */
 class HelloBlock extends BlockBase {
  
   public function access(AccountInterface $account) {
-//    return $account->hasPermission('access comments');
-    return true;
+    return $account->hasPermission('access content');
   }
 
  
   public function build() {
-//    return array('#markup' => 'Hello workd block text !');
-//    return array('#markup' => theme('hello_text', array('text' => 'anonymous')),);
-    return array('#markup' => theme('hello_text', array('text' => config('hello.settings')->get('hello_value'))),);
+    // Block level setting
+    $name = $this->configuration['hello_block_name'];
+    // Module global setting
+    $text = \Drupal::config('hello.settings')->get('hello_text');
+
+    $default_conf = $this->defaultConfiguration();
+    $default_name = $default_conf['hello_block_name'];
+    if (isset($name) && $name !== $default_name) {
+      $render_array = array(
+        '#theme' => 'hello_default',
+        '#hello_text' => String::checkPlain($text),
+        '#hello_name' => String::checkPlain($name)
+      );
+    } else {
+      $render_array = array(
+        '#markup' => 'No name configured yet'
+      );
+    }
+    return $render_array;
+
   }
  
+  public function blockSubmit($form, &$form_state) {
+    $this->configuration['hello_block_name'] = $form_state['values']['hello_block_name'];
+  }
+
+  public function blockForm($form, &$form_state) {
+    $form['hello_block_name'] = array(
+      '#type' => 'textfield',
+      '#title' => t('Name'),
+      '#size' => 15,
+      '#description' => t('Name of the person to greet'),
+      '#default_value' => $this->configuration['hello_block_name'],
+    );
+
+    return $form;
+  }
+
+  public function defaultConfiguration() {
+    return array(
+      'hello_block_name' => t('Default Name'),
+    );
+  }
+
 }
