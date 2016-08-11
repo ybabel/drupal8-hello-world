510cf80dc7224fb0ec2bd8b23c3402e0b6e6a540
ybabel <ybabel@free.fr>
2016-08-11 16:43:36 +0200

    Create à new route alter template : drupal generate:routesubscriber --module=hello

---
+++ src/Routing/RouteSubscriber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Drupal\hello\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RouteSubscriber.
+ *
+ * @package Drupal\hello\Routing
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function alterRoutes(RouteCollection $collection) {
+  }
+}
